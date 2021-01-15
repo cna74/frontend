7fe25679568f1c68d912948252225dee1a782a8b
cna74 <32127860+cna74@users.noreply.github.com>
2021-01-15 12:50:31 +0330

    rtl

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import * as  Icon from 'react-bootstrap-icons';
+import * as Icon from "react-bootstrap-icons";
 import "./index.css";
 
 const Header = () => {
   return (
-    <div className="container-fluid" dir="rtl">
-      <div className='container'>
+    <div className="container-fluid">
+      <div className="container">
         <div className="row">
           <div className="col-4 col-sm-3 col-md-3 col-lg-3 text-right">
             <a className="logo-head" href="#">
@@ -20,22 +20,52 @@ const Header = () => {
             </a>
           </div>
           <div className="col-12 col-sm-9 col-md-9 col-lg-9 d-flex justify-content-end align-items-center btn-head-holder">
-            <form action="" className='search-part' autoComplete="off">
-              <button className='search-part-btn'><Icon.Search /></button>
-              <input id="search_text" className='search-input' type="search" name="s" placeholder="جستجو کنید ..." autocomplete="off"></input>
+            <form action="" className="search-part" autoComplete="off">
+              <button className="search-part-btn">
+                <Icon.Search />
+              </button>
+              <input
+                id="search_text"
+                className="search-input"
+                type="search"
+                name="s"
+                placeholder="جستجو کنید ..."
+                autocomplete="off"
+              ></input>
             </form>
-            <a href="" className='cart-btn'>0 تومان
-            <button className=''><Icon.Cart /></button>
+            <a href="" className="cart-btn">
+              0 تومان
+              <button className="">
+                <Icon.Cart />
+              </button>
             </a>
           </div>
         </div>
       </div>
-      <ul>
-        <li><a className='link' href="default.asp">خانه</a></li>
-        <li><a className='link' href="news.asp">محصولات</a></li>
-        <li><a className='link' href="contact.asp">اخبار</a></li>
-        <li><a className='link' href="about.asp">درباره ما</a></li>
-      </ul>
+      <div className='container-fluid bottom-nav'>
+        <ul>
+          <li>
+            <a className="link" href="default.asp">
+              خانه
+            </a>
+          </li>
+          <li>
+            <a className="link" href="news.asp">
+              محصولات
+            </a>
+          </li>
+          <li>
+            <a className="link" href="contact.asp">
+              اخبار
+            </a>
+          </li>
+          <li>
+            <a className="link" href="about.asp">
+              درباره ما
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,14 +1,89 @@
 import React from "react";
+import { create } from "jss";
+import rtl from "jss-rtl";
+import {
+  StylesProvider,
+  jssPreset,
+  ThemeProvider,
+} from "@material-ui/core/styles";
+import CustomTheme from "./assets/CustomTheme";
+
 import ReactDom from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
-import Header from "./Header";
+import * as Icon from "react-bootstrap-icons";
 
-function Greeting() {
+// import Header from "./Header";
+
+const jss = create({ plugins: [...jssPreset().plugins, rtl] });
+
+function App() {
   return (
-    <React.Fragment>
-      <Header />
-      {/* <section className="container-fluid" dir="rtl">
+    <ThemeProvider theme={CustomTheme}>
+      <StylesProvider jss={jss}>
+      <div className="container-fluid">
+      <div className="container">
+        <div className="row">
+          <div className="col-4 col-sm-3 col-md-3 col-lg-3 text-right">
+            <a className="logo-head" href="#">
+              <img
+                className="img-fluid logo-head"
+                src="logo.png"
+                alt=""
+                width="25%"
+                height="25%"
+              />
+            </a>
+          </div>
+          <div className="col-12 col-sm-9 col-md-9 col-lg-9 d-flex justify-content-end align-items-center btn-head-holder">
+            <form action="" className="search-part" autoComplete="off">
+              <button className="search-part-btn">
+                <Icon.Search />
+              </button>
+              <input
+                id="search_text"
+                className="search-input"
+                type="search"
+                name="s"
+                placeholder="جستجو کنید ..."
+                autocomplete="off"
+              ></input>
+            </form>
+            <a href="" className="cart-btn">
+              0 تومان
+              <button className="">
+                <Icon.Cart />
+              </button>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className='container-fluid bottom-nav'>
+        <ul>
+          <li>
+            <a href="default.asp">
+              خانه
+            </a>
+          </li>
+          <li>
+            <a href="news.asp">
+              محصولات
+            </a>
+          </li>
+          <li>
+            <a href="contact.asp">
+              اخبار
+            </a>
+          </li>
+          <li>
+            <a href="about.asp">
+              درباره ما
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+        {/* <section className="container-fluid" dir="rtl">
         <div className="row">
           <div className="col-3">
             <h5>Filters</h5>
@@ -26,7 +101,8 @@ function Greeting() {
           </div>
         </div>
       </section> */}
-    </React.Fragment>
+      </StylesProvider>
+    </ThemeProvider>
   );
 }
 
@@ -40,4 +116,4 @@ const Card = ({ img, title }) => {
     </article>
   );
 };
-ReactDom.render(<Greeting />, document.getElementById("root"));
+ReactDom.render(<App />, document.getElementById("root"));
